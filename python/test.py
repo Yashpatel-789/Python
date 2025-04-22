ff690610aad00b92621a5997b55c5c15665441d4
Yashpatel-789 <pyash7480@gmail.com>
2025-04-22 22:04:17 +0530

    Add files via upload

---
+++ python/test.py
@@ -0,0 +1,11 @@
+a = 10
+b = 5
+
+# Logical AND
+print(a > 5 and b < 10)  # True and True -> True
+
+# Logical OR
+print(a < 5 or b < 10)   # False or True -> True
+
+# Logical NOT
+print(not (a == 10))     # not True -> False
